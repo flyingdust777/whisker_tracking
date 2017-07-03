7257aff0af0fa267b795f080c3ff242be257179c
Gautham Bommannan <gautham.bommannan@gmail.com>
2017-07-03 12:53:17 -0700

    added bare bones code to tracking

--- tracking.py
+++ tracking.py
@@ -5,3 +5,93 @@ Created on Mon Jul 03 12:14:19 2017
 @author: gauth
 """
 
+import cv2, sys
+import numpy as np
+print ("Modules Imported!")
+
+print ("Opening Video")
+video = cv2.VideoCapture('movie.mp4')
+if not video.isOpened():
+    print ("Could not open video")
+    sys.exit()
+
+#Try reading the first frame
+ok, frame = video.read()
+if not ok:
+    print ('Cannot read video file')
+    sys.exit()
+
+pts = 32 #default buffer
+counter=0
+
+whiskerLower = (100,100,100)
+whiskerUpper = (200,200,200)
+
+while True:
+    (grabbed, frame) = video.read()
+    if not grabbed:
+        break
+    frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    mask = cv2.inRange(hsv, whiskerLower, whiskerUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+    
+    #Now that we sort of have a map of the image, we need to highlight the whiskers
+    ##THIS BIT NEEDS TO CHANGE, SET FOR BALL TRACKING, NEED TO REDO SHAPE AND GO FOR 3 CONTOURS, NOT 1
+    if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+ 
+		# only proceed if the radius meets a minimum size
+		if radius > 10:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(frame, (int(x), int(y)), int(radius),
+				(0, 255, 255), 2)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+			pts.appendleft(center)
+    	# loop over the set of tracked points
+    for i in np.arange(1, len(pts)):
+		# if either of the tracked points are None, ignore
+		# them
+		if pts[i - 1] is None or pts[i] is None:
+			continue
+ 
+		# check to see if enough points have been accumulated in
+		# the buffer
+		if counter >= 10 and i == 1 and pts[-10] is not None:
+			# compute the difference between the x and y
+			# coordinates and re-initialize the direction
+			# text variables
+			dX = pts[-10][0] - pts[i][0]
+			dY = pts[-10][1] - pts[i][1]
+			(dirX, dirY) = ("", "")
+ 
+			# ensure there is significant movement in the
+			# x-direction
+			if np.abs(dX) > 20:
+				dirX = "East" if np.sign(dX) == 1 else "West"
+ 
+			# ensure there is significant movement in the
+			# y-direction
+			if np.abs(dY) > 20:
+				dirY = "North" if np.sign(dY) == 1 else "South"
+ 
+			# handle when both directions are non-empty
+			if dirX != "" and dirY != "":
+				direction = "{}-{}".format(dirY, dirX)
+ 
+			# otherwise, only one direction is non-empty
+			else:
+				direction = dirX if dirX != "" else dirY
